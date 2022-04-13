66fd355c83323b6fa587a9d3f1ae76c78ed9094a
LEE SEOWOO <76784643+leeseowoo@users.noreply.github.com>
2022-04-13 23:25:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 612 ms, Memory: 296096 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,28 +1,36 @@
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
-        Queue<Integer> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+            q.offer(i);
         }
 
         sb.append("<");
-        for (int i = 1; i <= n-1; i++) {
-            for (int j = 1; j <= k - 1; j++) {
-                queue.offer(queue.poll());
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                q.offer(q.poll());
             }
-            sb.append(queue.poll() + ", ");
+            sb.append(q.poll() + ", ");
         }
-        sb.append(queue.poll() + ">");
-        System.out.println(sb);
+
+        sb.append(q.poll() + ">");
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
     }
 }
