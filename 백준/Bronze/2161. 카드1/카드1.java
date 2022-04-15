b808d1c74455dc74fd8da13dac5869fd9e21f0a4
LEE SEOWOO <76784643+leeseowoo@users.noreply.github.com>
2022-04-15 22:39:58 +0900

    [Bronze I] Title: 카드1, Time: 140 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Bronze/2161. 카드1/카드1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            bw.write(q.poll() + " ");
+            q.offer(q.poll());
+        }
+
+        bw.write(q.poll() + "");
+        bw.flush();
+    }
+}
