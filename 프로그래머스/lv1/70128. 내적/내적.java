1318a514050aa010a847ec46dd9a71dc60597773
LEE SEOWOO <76784643+leeseowoo@users.noreply.github.com>
2022-06-20 14:05:27 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = a.length-1; i >= 0; i--) {
+            answer += a[i]*b[i];
+        }
+        
+        return answer;
+    }
+}
