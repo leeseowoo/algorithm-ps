e0c0857dde9a5b7c6d946bf5e69484f6b2230de6
LEE SEOWOO <76784643+leeseowoo@users.noreply.github.com>
2022-07-22 21:36:14 +0900

    [Gold V] Title: 토마토, Time: 664 ms, Memory: 120444 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,132 +1,94 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int m, n;
-    static int[][] storage;
-    static int[] dx = new int[]{-1, 1, 0, 0};
-    static int[] dy = new int[]{0, 0, -1, 1};
-    static boolean[][] visited;
-    static boolean[][] anotherVisited;
-    static Queue<int[]> queue = new LinkedList<>();
-    static Queue<int[]> anotherQueue = new LinkedList<>();
+    static int[][] map;
+    static int n, m;
+    static Queue<int []> q = new LinkedList<>();
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        storage = new int[n][m];
-        visited = new boolean[n][m];
-        anotherVisited = new boolean[n][m];
+        map = new int[n][m];
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<m; j++) {
-                storage[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (storage[i][j] == 1) {
-                    queue.offer(new int[]{i, j});
-                    anotherQueue.offer(new int[]{i, j});
-                    visited[i][j] = true;
-                    anotherVisited[i][j] = true;
-                }
-            }
-        }
-
-        int days = 0;
-        int status = checkStatus();
-
-        if (status == 1) {
-            days = bfs() - 1;
+        if(firstStep()) {       // 익은 토마토 위치 큐에 추가, 안 익은 토마토가 있으면 true 반환
+            bfs();
+            lastStep();         // 안 익은 토마토가 남아있는지 확인
         } else {
-            days = status;
+            answer = 0;         // 이미 모두 익어있는 경우 0
         }
 
-        bw.write(days + "");
+        bw.write(answer + "");
         bw.flush();
     }
 
-    public static int checkStatus() {
-        boolean foundZero = false;
-
-        while (!anotherQueue.isEmpty()) {
-            int[] currnet = anotherQueue.poll();
-            int x = currnet[0];
-            int y = currnet[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx <= -1 || ny <= -1 || nx >= n || ny >= m) {
-                    continue;
-                }
-
-                if (storage[x][y] == -1 && storage[nx][ny] == 0) {
-                    continue;
-                }
-
-                if (!anotherVisited[nx][ny]) {
-                    anotherVisited[nx][ny] = true;
-                    anotherQueue.offer(new int[]{nx, ny});
-                    if (storage[nx][ny] == 0) {
-                        foundZero = true;
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!anotherVisited[i][j]) {
-                    return -1;
-                }
-            }
-        }
-
-        if (!foundZero) {
-            return 0;
-        }
-
-        return 1;
-    }
-
-    public static int bfs() {
+    static void bfs() {
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
         int days = 0;
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            days = storage[x][y];
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            days = map[x][y];
 
-            for (int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx <= -1 || ny <= -1 || nx >= n || ny >= m) {
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
                     continue;
                 }
 
-                if (!visited[nx][ny]) {
-                    visited[nx][ny] = true;
-
-                    if (storage[nx][ny] == 0) {
-                        storage[nx][ny] = storage[x][y] + 1;
-                        queue.offer(new int[]{nx, ny});
-                    }
+                if(map[nx][ny] == 0) {
+                    map[nx][ny] = days + 1;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
 
-        return days;
+        answer = days - 1;
+    }
+
+    static boolean firstStep() {
+        boolean foundZero = false;
+
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(map[i][j] == 1) {
+                    q.offer(new int[]{i, j});
+                } else if(map[i][j] == 0) {
+                    foundZero = true;
+                }
+            }
+        }
+
+        return foundZero;
+    }
+
+    static void lastStep() {
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(map[i][j] == 0) {
+                    answer = -1;
+                    return;
+                }
+            }
+        }
     }
 }
